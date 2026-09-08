5a94a76134bed90d71007486fd4a3bf18f806d1e
Luo, Can Yu (C.) <cluo28@ford.com>
2026-09-08 20:10:13 +0800

    recreate admin content on route change

--- admin-web/src/views/Layout.vue
+++ admin-web/src/views/Layout.vue
@@ -68,16 +68,18 @@
       </el-header>
 
       <el-main class="main">
-        <router-view v-slot="{ Component }">
-          <component :is="Component" :key="route.fullPath" />
-        </router-view>
+        <div v-if="contentVisible" :key="contentKey" class="content-view">
+          <router-view v-slot="{ Component }">
+            <component :is="Component" :key="contentKey" />
+          </router-view>
+        </div>
       </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, nextTick, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import { useUserStore } from '@/store/user'
@@ -87,9 +89,18 @@ const router = useRouter()
 const userStore = useUserStore()
 
 const isCollapse = ref(false)
+const contentKey = ref(0)
+const contentVisible = ref(true)
 
 const activeMenu = computed(() => route.path)
 const currentRoute = computed(() => route.meta?.title || '')
+
+watch(() => route.fullPath, async () => {
+  contentVisible.value = false
+  await nextTick()
+  contentKey.value += 1
+  contentVisible.value = true
+})
 const userName = computed(() => userStore.userInfo?.name || '管理员')
 const userAvatar = computed(() => {
   const role = userStore.userInfo?.role
